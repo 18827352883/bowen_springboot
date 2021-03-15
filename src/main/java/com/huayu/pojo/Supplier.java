9dfbf6571e5c96aff70f44d23f4830f42e7dbeeb
Your Name <Hbw2466512236@163.com>
2021-03-15 16:14:36 +0800

    修改时间

--- src/main/java/com/huayu/pojo/Supplier.java
+++ src/main/java/com/huayu/pojo/Supplier.java
@@ -3,6 +3,7 @@ package com.huayu.pojo;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -22,6 +23,7 @@ public class Supplier {
     private String name;
     private String phone;   //电话
     private String wechat;  //微信或者QQ
+    @JsonFormat(pattern = "yyyy-MM-dd",timezone = "GMT+8")
     private Date createTime;
     private String remarks;
 }
